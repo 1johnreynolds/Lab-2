897a44ab43f8d47555530b5a4ec6e95b849c2320
Derek(Jianyu) Shen <huasucaster@gmail.com>
2021-03-03 01:28:32 -0600

    query model

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -51,9 +51,9 @@ public class HomeController extends Controller {
         return publicationForm.get().checkAuthorized().thenApplyAsync((WSResponse r) -> {
             if (r.getStatus() == 200 && r.asJson() != null && !r.getBody().equals("null")) {
                 JsonNode res = r.asJson();
-                System.out.println(res);
-                System.out.println(res.getNodeType());
-                System.out.println(res.size());
+//                System.out.println(res);
+//                System.out.println(res.getNodeType());
+//                System.out.println(res.size());
                 // add Title to session
                 session("Title", publicationForm.get().getTitle());   // store Title in session for your project
                 // session("Id", publicationForm.get().getId());
@@ -62,28 +62,29 @@ public class HomeController extends Controller {
                 String query1 = "";
                 for (int i = 0; i < res.size(); i++) {
                     //JsonObject jsonObject = new JsonParser().parse(res.get(Integer.toString(i)).toString()).getAsJsonObject();
-                    System.out.println(res.get(i));
+                    //System.out.println(res.get(i));
                     JsonNode row = res.get(i);
-                    System.out.println(row.findValue("pid").asText());
+                    //System.out.println(row.findValue("pid").asText());
                     query1 += "The publication you are looking for: \n" +
-                            "PID: " + row.findValue("pid") + "\n" +
+//                            "PID: " + row.findValue("pid") + "\n" +
                             "Title: " + row.findValue("title") + "\n" +
-                            "mdate: " + row.findValue("mdate") + "\n" +
-                            "author: " + row.findValue("author") + "\n" +
-                            "author list: " + row.findValue("author_list") + "\n" +
-                            "article key: " + row.findValue("article_key") + "\n" +
-                            "editor: " + row.findValue("editor") + "\n" +
-                            "pages: " + row.findValue("pages") + "\n" +
+                            "Mdate: " + row.findValue("mdate") + "\n" +
+//                            "Author: " + row.findValue("author") + "\n" +
+                            "Author list: " + row.findValue("author_list") + "\n" +
+                            "Article key: " + row.findValue("article_key") + "\n" +
+                            "Editors: " + row.findValue("editors") + "\n" +
+                            "Pages: " + row.findValue("pages") + "\n" +
                             "ee: " + row.findValue("ee") + "\n" +
-                            "pub_url: " + row.findValue("pub_url") + "\n" +
-                            "journal: " + row.findValue("journal") + "\n" +
-                            "book title: " + row.findValue("book_title") + "\n" +
-                            "volume: " + row.findValue("volume") + "\n" +
-                            "pub_number: " + row.findValue("pub_number") + "\n" +
-                            "publisher: " + row.findValue("publisher") + "\n" +
+                            "Pub_url: " + row.findValue("pub_url") + "\n" +
+                            "Pub_year: " + row.findValue("pub_year") + "\n" +
+                            "Journal: " + row.findValue("journal") + "\n" +
+                            "Book_title(Conference_name): " + row.findValue("book_title") + "\n" +
+                            "Volume: " + row.findValue("volume") + "\n" +
+                            "Pub_number: " + row.findValue("pub_number") + "\n" +
+                            "Publisher: " + row.findValue("publisher") + "\n" +
                             "ISBN: " + row.findValue("isbn") + "\n" +
-                            "series: " + row.findValue("series") + "\n" +
-                            "cross_ref: " + row.findValue("cross_ref") + "\n";
+                            "Series: " + row.findValue("series") + "\n" +
+                            "Cross_ref: " + row.findValue("cross_ref") + "\n";
                     System.out.println(query1);
                 }
                 return ok(views.html.query1.render(query1));
